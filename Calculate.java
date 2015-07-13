9c36282b061567919da6f9d9ed8587cef178f66f
vfilipchenko <vfilipchenko@gmail.com>
2015-07-13 16:19:09 +0300

    add new Calculate

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,10 @@ public class Calculate {
 
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("Summ = "+ summ);
 	}
 
 }
